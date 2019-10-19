268ad0c241ddd485fd8dc2e7aaa4741c82733700
William SIMON--VEZO <william.simonvezo@gmail.com>
2019-10-19 14:06:38 +0200

    Curriculum Vitae

---
+++ README.md
@@ -0,0 +1,2 @@
+# CurriculumVitae
+[Curriculum vitae](https://varadiell.github.io/CurriculumVitae/)
---
+++ vantaJs/custom.js
@@ -0,0 +1,5 @@
+VANTA.NET({
+  el: "#vantaJs",
+  // color: 0x0,
+  // backgroundColor: 0xffffff
+});
