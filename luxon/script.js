f672363a17dba758022bc28a4c0526274e60f88a
William SIMON--VEZO <william.simonvezo@gmail.com>
2025-02-25 17:57:20 +0100

    added ml/dl

--- luxon/script.js
+++ luxon/script.js
@@ -1,5 +1,7 @@
 const age = Math.floor(Math.abs(luxon.DateTime.fromFormat('20.03.1993', 'dd.MM.yyyy').diffNow('years').years));
+const experience = Math.floor(Math.abs(luxon.DateTime.fromFormat('01.11.2014', 'dd.MM.yyyy').diffNow('years').years));
 const year = luxon.DateTime.local().year;
 
 document.getElementById('luxonAge').innerHTML = age;
+document.getElementById('luxonExperience').innerHTML = experience;
 document.getElementById('luxonYear').innerHTML = year;
